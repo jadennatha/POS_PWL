993162678e0075e0dd8bc6b1b33bce3d6ee19b87
jadennatha <144076853+jadennatha@users.noreply.github.com>
2025-03-12 19:42:28 +0700

    jb4

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -5,20 +5,27 @@
     </head>
     <body>
         <h1>Data User</h1>
+        <a href="/user/tambah">+ Tambah User</a>
         <table border="1" cellpadding="2" cellspacing="0">
             <tr>
-                <th>ID</th>
-                <th>Username</th>
-                <th>Nama</th>
-                <th>ID Level Pengguna</th>
-                </tr>
-            @foreach ($data as $d)
-            <tr>
-                <td>{{ $d->user_id }}</td>
-                <td>{{ $d->username }}</td>
-                <td>{{ $d->nama }}</td>
-                <td>{{ $d->level_id }}</td>
+                <td>ID</td>
+                <td>Username</td>
+                <td>Nama</td>
+                <td>ID Level Pengguna</td>
+                <td>kode level</td>
+                <td>nama level</td>
+                <td>Aksi</td>
             </tr>
+            @foreach ($data as $d)
+                <tr>
+                    <td>{{ $d->user_id }}</td>
+                    <td>{{ $d->username }}</td>
+                    <td>{{ $d->nama }}</td>
+                    <td>{{ $d->level_id }}</td>
+                    <td>{{ $d->level->level_kode }}</td>
+                    <td>{{ $d->level->level_nama }}</td>
+                    <td><a href="/user/ubah/{{ $d->user_id }}">ubah</a> | <a href="/user/hapus/{{ $d->user_id }}">hapus</a></td>
+                </tr>
             @endforeach
         </table>
     </body>
