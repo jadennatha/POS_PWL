944e0c3b713cc8460f872b83bd8f959798a68aba
jadennatha <144076853+jadennatha@users.noreply.github.com>
2025-05-05 15:04:31 +0700

    js 7

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -58,4 +58,9 @@ class UserController extends Controller
 
         return redirect('/user');
     }
+
+    public function register()
+    {
+        return view('register');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,66 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\BarangController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\POSController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\POSController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::pattern('id', '[0-9]+'); // artinya ketika ada parameter {id}, maka harus berupa angka
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('login', [AuthController::class, 'login'])->name('login');
+Route::post('login', [AuthController::class, 'postlogin']);
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
+
+Route::middleware(['auth'])->group(function () { // artinya semua route di dalam group ini harus login dulu
+    // masukkan semua route yang perlu autentikasi di sini
 });
 
+// Route::get('/', [HomeController::class, 'index']);
+Route::post('/welcome', [WelcomeController::class, 'index'])->name('home');
 Route::get('/level', [LevelController::class, 'index']);
-Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/user', [UserController::class, 'index']);
-Route::get('/user/tambah', [UserController::class, 'tambah']);
-Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-Route::put('/user/ubah_simpan/{id}', [userController::class, 'ubah_simpan']);
-Route::get('/user/hapus/{id}', [userController::class, 'hapus']);
-Route::get('/kategori/create', [KategoriController::class, 'create']);
-Route::post('/kategori', [KategoriController::class, 'store']);
+Route::post('/kategori', [KategoriController::class, 'store'])->name('kategori.store');
+
+Route::get('/kategori/create', [KategoriController::class, 'create'])->name('kategori.create');
+
+
 Route::get('/kategori/edit/{id}', [KategoriController::class, 'edit'])->name('kategori.edit');
 Route::post('/kategori/update/{id}', [KategoriController::class, 'update'])->name('kategori.update');
-Route::delete('/kategori/delete/{id}', [KategoriController::class, 'hapus'])->name('kategori.hapus');
+
+Route::delete('/kategori/delete/{id}', [KategoriController::class, 'destroy'])->name('kategori.destroy');
+
 Route::resource('m_user', POSController::class);
+
+Route::get('/user', [UserController::class, 'index']);
+
+Route::get('/user/tambah', [UserController::class, 'tambah']);
+Route::get('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+Route::get('/user/register', [UserController::class, 'register']); 
+
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+
+Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+
+Route::get('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+
+Route::middleware(['auth'])->group(function () { // artinya semua route di dalam group ini harus login dulu
+    Route::get('/', [WelcomeController::class, 'index']);
+    // route Level
+
+    // artinya semua route di dalam group ini harus punya role ADM (Administrator)
+    Route::middleware(['authorize:ADM'])->group(function () {
+        Route::get('/level', [LevelController::class, 'index']);
+        Route::post('/level/list', [LevelController::class, 'list']); // untuk untuk list json datatables
+        Route::get('/level/create', [LevelController::class, 'create']);
+        Route::post('/level/store', [LevelController::class, 'store']);
+        Route::get('/level/{id}/edit', [LevelController::class, 'edit']); // untuk tampilkan form edit
+        Route::put('/level/{id}', [LevelController::class, 'update']); // untuk proses update data
+        Route::delete('/level/{id}', [LevelController::class, 'destroy']); // untuk proses hapus data
+    });
+
+    
+});
